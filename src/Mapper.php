6600cbe10de29e327e2dc00913d50a8a74a64a71
Will Milton <wa.milton@gmail.com>
2014-06-24 17:51:33 -0500

    tangling files and custom code

--- src/Mapper.php
+++ src/Mapper.php
@@ -3,19 +3,54 @@
 namespace Drupal\Tangler;
 
 use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
 use Composer\Installer\InstallationManager;
 use Composer\Repository\RepositoryManager;
 
 class Mapper
 {
-    private $fs = false;
+    private $fs     = false;
+    private $finder = false;
+    private $root   = false;
 
     private function getFS()
     {
         return $this->fs ? $this->fs : $this->fs = new Filesystem();
     }
 
+    private function getRoot()
+    {
+        if ($this->root) {
+            return $this->root;
+        }
+        $paths = ['/'];
+        foreach(array_filter(explode('/', getcwd())) as $dir) {
+            $paths[] = end($paths).$dir.'/';
+        }
+        $fs = $this->getFS();
+        foreach (array_reverse($paths) as $path) {
+            if ($fs->exists($path.'/composer.json')) {
+                return $this->root = $path;
+            }
+        }
+    }
+
+    private function getFinder()
+    {
+        return new Finder();
+    }
+
     public function getMap(InstallationManager $im, RepositoryManager $rm)
+    {
+        return array_merge(
+            $this->mapContrib($im, $rm),
+            $this->mapCustom(),
+            $this->mapSettings(),
+            $this->mapFiles()
+        );
+    }
+
+    public function mapContrib(InstallationManager $im, RepositoryManager $rm)
     {
         $typePathMap = $this->getTypePathMap();
         $typeInstallMap = [];
@@ -26,21 +61,69 @@ class Mapper
                 $name = explode('/', $package->getPrettyName())[1];
                 $installPath = $im->getInstaller($package->getType())
                     ->getInstallPath($package);
-                $typeInstallMap[$drupalType][] = [
-                    $installPath => sprintf($typePathMap[$drupalType], $name)
-                ];
+                $typeInstallMap[$drupalType][$installPath] = sprintf($typePathMap[$drupalType], $name);
             }
         }
         return array_intersect_key($typeInstallMap, $typePathMap);
     }
 
+    public function mapCustom()
+    {
+        $root   = $this->getRoot();
+        $custom = "$root/drupal";
+        $fs     = $this->getFS();
+        $paths  = [];
+        foreach (['module', 'theme', 'profile'] as $type) {
+            if ($fs->exists("$custom/{$type}s")) {
+                $finder = $this->getFinder()
+                    ->ignoreUnreadableDirs()
+                    ->depth('== 0')
+                    ->in("$custom/{$type}s");
+                foreach ($finder as $file) {
+                    $install = $fs->makePathRelative($file->getRealpath(), $root);
+                    $paths["custom_$type"][$install] = sprintf(
+                        $this->getTypePathMap($type),
+                        $file->getFilename()
+                    );
+                }
+            }
+        }
+        return $paths;
+    }
+
+    public function mapFiles()
+    {
+        return [
+            'files' => ['cnf/files' => 'www/sites/default/files']
+        ];
+    }
+
+    public function mapSettings()
+    {
+        return [
+            'settings' => ['cnf/settings.php' => 'www/sites/default/settings.php']
+        ];
+    }
+
     public function mirror($map)
     {
         $fs = $this->getFS();
-        foreach ($map as $type => $pathMaps) {
-            foreach ($pathMaps as $pathMap) {
-                foreach ($pathMap as $installPath => $targetPath) {
-                    $fs->mirror($installPath, $targetPath);
+        $root = $this->getRoot();
+        foreach ($map as $type => $pathMap) {
+            foreach ($pathMap as $installPath => $targetPath) {
+                if ($fs->exists("$root/$installPath")) {
+                    $dest = rtrim($fs->makePathRelative(
+                        rtrim("$root$targetPath", '/'),
+                        getcwd()
+                    ), '/');
+                    $fs->symlink(
+                        $fs->makePathRelative(
+                            rtrim("$root$installPath", '/'),
+                            $dest
+                        ),
+                        $dest,
+                        true
+                    );
                 }
             }
         }
@@ -51,14 +134,18 @@ class Mapper
         $this->getFS()->remove(['directory', $this->getTypePathMap()['core']]);
     }
 
-    public function getTypePathMap()
+    public function getTypePathMap($type = null)
     {
-        return [
+        $map = [
             'core'    => 'www',
-            'module'  => 'www/sites/all/modules/%s',
+            'module'  => 'www/sites/all/modules/contrib/%s',
             'theme'   => 'www/sites/all/themes/%s',
             'profile' => 'www/profiles/%s'
         ];
+        if ($type) {
+            return $map[$type];
+        }
+        return $map;
     }
 
     public function getDrupalType($package)
